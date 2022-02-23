9f12291bf709000510325d3f211eae141495f9ed
Gaétan Basile <gaetan.basile@mink-agency.com>
2022-02-23 19:14:39 +0100

    🔧 Add routes for test results

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   scope :api do
     scope :v1 do
       resources :students
-      resources :tests
+      resources :tests do
+        resources :results, controller: 'test_results'
+
+        member do
+          get 'stats'
+        end
+      end
     end
   end
 end
